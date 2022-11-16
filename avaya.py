53dda6de5eebf0d0fa34e256beb52e22061121b2
David Abol Ger <draker74@gmail.com>
2022-11-16 17:36:41 +0300

    Update avaya.py

---
+++ avaya.py
@@ -0,0 +1,23 @@
+import requests
+from plotly.graph_objs import Bar
+from plotly import offline
+
+site_url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+response = requests.get(site_url, headers=headers)
+response_json = response.json()
+
+repo_names, repo_stars = [], []
+
+for repo in response_json['items']:
+    repo_names.append(repo['name'])
+    repo_stars.append(repo['stargazers_count'])
+
+data_plots = [{'type' : 'bar', 'x':repo_names , 'y': repo_stars}]
+layout = {'title': 'GItHubs Most Popular Javascript Projects',
+'xaxis': {'title': 'Repository'},
+'yaxis': {'title': 'Stars'}}
+
+fig = {'data': data_plots, 'layout': layout}
+offline.plot(fig, filename='Most_Popular_JavaScript_Repos.html')
